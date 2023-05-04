c173229acd678d4f3424a821000ef36c89a39552
Nayan026 <nayan6260@gmail.com>
2023-05-04 23:08:34 +0530

    gradient added

--- lib/StatPage.dart
+++ lib/StatPage.dart
@@ -61,6 +61,7 @@ class _StatPageState extends State<StatPage> {
                       opacity: 0.6,
                       child: Container(
                         decoration: BoxDecoration(
+                         
                           image: DecorationImage(
                             image: AssetImage('images/image1.jpg'),
                             fit: BoxFit.cover,
@@ -73,20 +74,34 @@ class _StatPageState extends State<StatPage> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Container(
-                            padding: EdgeInsets.all(10),
+                            padding: const EdgeInsets.all(10),
                             decoration: BoxDecoration(
+                               gradient: LinearGradient(
+                            // Where the linear gradient begins and ends
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            // Add one stop for each color. Stops should increase from 0 to 1
+                            stops: [0.1, 0.5, 0.7, 0.9],
+                            colors: [
+                              // Colors are easy thanks to Flutter's Colors class.
+                              Colors.orange[800]!,
+                              Colors.orange[700]!,
+                              Colors.orange[600]!,
+                              Colors.orange[400]!,
+                            ],
+                          ),
                               border: Border.all(color: Colors.white),
                               borderRadius: BorderRadius.circular(10),
                             ),
                             child: Text(
                               '$question1',
                               textScaleFactor: 2.0,
-                              style: TextStyle(
+                              style: const TextStyle(
                                   color: Colors.black, fontSize: 50.0),
                             ),
                           ),
-                          SizedBox(height: 10),
-                          Text(
+                          const SizedBox(height: 10),
+                          const Text(
                             'Total no. of people fed',
                             style: TextStyle(
                               color: Colors.black,
@@ -129,22 +144,36 @@ class _StatPageState extends State<StatPage> {
                           Container(
                             padding: EdgeInsets.all(10),
                             decoration: BoxDecoration(
+                               gradient: LinearGradient(
+                            // Where the linear gradient begins and ends
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            // Add one stop for each color. Stops should increase from 0 to 1
+                            stops: [0.1, 0.5, 0.7, 0.9],
+                            colors: [
+                              // Colors are easy thanks to Flutter's Colors class.
+                              Colors.orange[800]!,
+                              Colors.orange[700]!,
+                              Colors.orange[600]!,
+                              Colors.orange[400]!,
+                            ],
+                          ),
                               border: Border.all(color: Colors.white),
                               borderRadius: BorderRadius.circular(10),
                             ),
                             child: Text(
                               '$question2',
                               textScaleFactor: 2.0,
-                              style: TextStyle(
+                              style: const TextStyle(
                                   color: Colors.black, fontSize: 50.0),
                             ),
                           ),
-                          SizedBox(height: 10),
-                          Text(
+                          const SizedBox(height: 7),
+                          const Text(
                             'Kgs of food wastage reduced',
                             style: TextStyle(
                               color: Colors.black,
-                              fontSize: 24.0,
+                              fontSize: 14.0,
                               fontWeight: FontWeight.bold,
                             ),
                             textAlign: TextAlign.center,
@@ -156,8 +185,8 @@ class _StatPageState extends State<StatPage> {
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
+            const SizedBox(height: 20),
             Expanded(
               child: Container(
                 padding: EdgeInsets.all(10),
@@ -169,7 +198,7 @@ class _StatPageState extends State<StatPage> {
                     Opacity(
                       opacity: 0.7,
                       child: Container(
-                        decoration: BoxDecoration(
+                        decoration: const BoxDecoration(
                           image: DecorationImage(
                             image: AssetImage('images/image3.jpg'),
                             fit: BoxFit.cover,
@@ -184,18 +213,32 @@ class _StatPageState extends State<StatPage> {
                           Container(
                             padding: EdgeInsets.all(10),
                             decoration: BoxDecoration(
+                               gradient: LinearGradient(
+                            // Where the linear gradient begins and ends
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            // Add one stop for each color. Stops should increase from 0 to 1
+                            stops: [0.1, 0.5, 0.7, 0.9],
+                            colors: [
+                              // Colors are easy thanks to Flutter's Colors class.
+                              Colors.orange[800]!,
+                              Colors.orange[700]!,
+                              Colors.orange[600]!,
+                              Colors.orange[400]!,
+                            ],
+                          ),
                               border: Border.all(color: Colors.white),
                               borderRadius: BorderRadius.circular(10),
                             ),
                             child: Text(
                               '$question3',
                               textScaleFactor: 2.0,
-                              style: TextStyle(
+                              style: const TextStyle(
                                   color: Colors.black, fontSize: 50.0),
                             ),
                           ),
-                          SizedBox(height: 10),
-                          Text(
+                          const SizedBox(height: 10),
+                          const Text(
                             'Litres of water saved',
                             style: TextStyle(
                               color: Colors.black,
